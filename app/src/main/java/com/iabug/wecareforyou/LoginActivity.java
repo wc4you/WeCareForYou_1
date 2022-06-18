11fa4fac0cee52a97f42befa07e9d07780a90580
Vrk20 <kv192000@gmail.com>
2022-06-19 00:13:54 +0530

    commit 1

--- app/src/main/java/com/iabug/wecareforyou/LoginActivity.java
+++ app/src/main/java/com/iabug/wecareforyou/LoginActivity.java
@@ -103,7 +103,7 @@ public class LoginActivity extends AppCompatActivity {
                                         Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
                                     }
                                 } else {
-                                    Intent intent = new Intent(LoginActivity.this, QuizActivity.class);
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                                     startActivity(intent);
                                     finish();
                                 }
